1791d3b654fc6e5c527f936d692e89565da6eee6
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-17 17:11:44 +0100

    New file for constants

---
+++ Completion handler/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Completion handler
+//
+//  Created by Macbook Air 2017 on 17. 12. 2023..
+//
+
+import Foundation
+
+let GET_USERS_ENDPOINT = "https://jsonplaceholder.typicode.com/users"
