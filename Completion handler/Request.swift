578cf92ccee4f59a0b2696e1aad6aab9b21b083c
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-17 18:01:27 +0100

    Implemented getUsers function
    - with @escaping completion handler

--- Completion handler/Request.swift
+++ Completion handler/Request.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 class Request {
-    
+    static func getUsers(completion: @escaping ([User]) -> Void) {
+        
+        guard let url = URL(string: GET_USERS_ENDPOINT) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            
+            guard let unwrappedData = data else {
+                return
+            }
+            do {
+                let users = try JSONDecoder().decode([User].self, from: unwrappedData)
+                completion(users)
+            } catch {
+                return
+            }
+        })
+        task.resume()
+    }
 }
